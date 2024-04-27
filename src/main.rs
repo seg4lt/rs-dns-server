a2a1f70ede0fbf038ed6a0de356a8755fe423528
seg4lt <12027886+seg4lt@users.noreply.github.com>
2024-04-26 22:29:22 -0500

    Uncomment code
    Uncomment lines

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
-// Uncomment this block to pass the first stage
-// use std::net::UdpSocket;
+use std::net::UdpSocket;
+
+use tracing::{error, info};
+
+use crate::config::setup_log;
+
+mod config;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    setup_log().expect("Failed to setup log");
+    info!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    // let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
-    // let mut buf = [0; 512];
-    //
-    // loop {
-    //     match udp_socket.recv_from(&mut buf) {
-    //         Ok((size, source)) => {
-    //             println!("Received {} bytes from {}", size, source);
-    //             let response = [];
-    //             udp_socket
-    //                 .send_to(&response, source)
-    //                 .expect("Failed to send response");
-    //         }
-    //         Err(e) => {
-    //             eprintln!("Error receiving data: {}", e);
-    //             break;
-    //         }
-    //     }
-    // }
+    let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
+    let mut buf = [0; 512];
+
+    loop {
+        match udp_socket.recv_from(&mut buf) {
+            Ok((size, source)) => {
+                info!("Received {} bytes from {}", size, source);
+                let response = [];
+                udp_socket
+                    .send_to(&response, source)
+                    .expect("Failed to send response");
+            }
+            Err(e) => {
+                error!("Error receiving data: {}", e);
+                break;
+            }
+        }
+    }
 }
